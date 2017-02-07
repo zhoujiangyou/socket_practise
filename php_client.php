b6a8d03303c1249bc65fe7742f2712f5ba449c91
coder油 <935468215@qq.com>
2017-02-07 15:06:21 +0800

    Add files via upload

---
+++ php_client.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/2/6
+ * Time: 15:55
+ */
+set_time_limit(0);
+
+$host = "127.0.0.1";
+$port = 2046;
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)or die("Could not create  socket\n"); // 创建一个Socket
+
+$connection = socket_connect($socket, $host, $port) or die("Could not connet server\n");    //  连接
+socket_write($socket, "hello socket") or die("Write failed\n"); // 数据传送 向服务器发送消息
+while ($buff = socket_read($socket, 1024, PHP_NORMAL_READ)) {
+    echo("Response was:" . $buff . "\n");
+}
+socket_close($socket);
